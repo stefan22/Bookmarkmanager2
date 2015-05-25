b23aacafe42c597fca3a7f71fc5f0be7be48659c
Stefan Lissia <stefan.emailacct@gmail.com>
2015-05-25 21:48:41 +0100

    changed a few files

--- server.rb
+++ server.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'data_mapper'
 
+enable :sessions
+set :session_secret, 'super secret'
+
 env = ENV['RACK_ENV'] || 'development'
 
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
@@ -8,6 +11,7 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
 require './lib/tag'
+require './lib/user'
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -40,6 +44,28 @@ class Bookmarkmanager2 < Sinatra::Base
     erb :index
 
   end
+
+  get '/users/new' do
+
+  erb :'users/new'
+  end
+
+  post '/users' do
+    Users.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+
+  helpers do
+
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
+
+  end
+
+
 
 
 
